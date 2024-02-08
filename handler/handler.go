d38f0fec3dfca76687009d2ab67b3e157416eb06
WhatACotton <whatacotton@gmail.com>
2024-02-09 00:12:45 +0900

    fix: delete unused props

--- handler/handler.go
+++ handler/handler.go
@@ -38,21 +38,21 @@ func (h *Handler) SetupRoutesForItem() {
 	{
 		itemGroup.GET("", func(c *gin.Context) {
 			// レスポンスの処理
-			PreviewList := items.GetPreviewList(c, items.ItemRequests{})
+			PreviewList := items.GetPreviewList(items.ItemRequests{})
 			c.JSON(200, PreviewList)
 		})
 		itemGroup.GET("/:item_id", func(c *gin.Context) {
 
 			// item_id の取得
 			itemId := c.Param("item_id")
-			Overview := items.GetOverview(c, items.ItemRequests{}, itemId)
+			Overview := items.GetOverview(items.ItemRequests{}, itemId)
 			// レスポンスの処理
 			c.JSON(200, Overview)
 		})
 		itemGroup.GET("/search", func(c *gin.Context) {
 			keywords := c.Query("keyword")
 			log.Println(keywords)
-			PreviewList := items.GetSearchPreviewList(c, items.ItemRequests{}, strings.Split(keywords, "+"))
+			PreviewList := items.GetSearchPreviewList(items.ItemRequests{}, strings.Split(keywords, "+"))
 			c.JSON(200, PreviewList)
 		})
 	}
