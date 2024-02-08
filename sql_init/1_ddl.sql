3e9d1a33425793faae0f25a589eb5a160ebd4069
WhatACotton <whatacotton@gmail.com>
2024-02-08 18:42:14 +0900

    add: mount volume and update database

--- sql_init/1_ddl.sql
+++ sql_init/1_ddl.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+-- MariaDB dump 10.19-11.2.2-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: CharisWorks
 -- ------------------------------------------------------
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -15,17 +15,13 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE IF NOT EXISTS `CharisWorks`;
-
-USE `CharisWorks`;
-
 --
 -- Table structure for table `carts`
 --
 
 DROP TABLE IF EXISTS `carts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `carts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `purchaser_user_id` varchar(100) NOT NULL,
@@ -40,12 +36,21 @@ CREATE TABLE `carts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `carts`
+--
+
+LOCK TABLES `carts` WRITE;
+/*!40000 ALTER TABLE `carts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `history_items`
 --
 
 DROP TABLE IF EXISTS `history_items`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `history_items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
@@ -57,10 +62,19 @@ CREATE TABLE `history_items` (
   `description` text DEFAULT NULL,
   `tags` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `history_items_items_FK` (`item_id`),
-  CONSTRAINT `history_items_items_FK` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
+  UNIQUE KEY `history_items_unique` (`item_id`),
+  KEY `history_items_items_FK` (`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `history_items`
+--
+
+LOCK TABLES `history_items` WRITE;
+/*!40000 ALTER TABLE `history_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `history_items` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `history_users`
@@ -68,7 +82,7 @@ CREATE TABLE `history_items` (
 
 DROP TABLE IF EXISTS `history_users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `history_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(100) NOT NULL,
@@ -77,10 +91,18 @@ CREATE TABLE `history_users` (
   `description` text DEFAULT NULL,
   `created_at` timestamp NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `history_users_users_FK` (`user_id`),
-  CONSTRAINT `history_users_users_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  KEY `history_users_users_FK` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `history_users`
+--
+
+LOCK TABLES `history_users` WRITE;
+/*!40000 ALTER TABLE `history_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `history_users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `items`
@@ -88,7 +110,7 @@ CREATE TABLE `history_users` (
 
 DROP TABLE IF EXISTS `items`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `manufacturer_user_id` varchar(100) NOT NULL,
@@ -96,10 +118,18 @@ CREATE TABLE `items` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `history_item_id` (`history_item_id`),
   KEY `items_users_FK` (`manufacturer_user_id`),
-  CONSTRAINT `items_history_items_FK` FOREIGN KEY (`history_item_id`) REFERENCES `history_items` (`id`),
   CONSTRAINT `items_users_FK` FOREIGN KEY (`manufacturer_user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `shippings`
@@ -107,7 +137,7 @@ CREATE TABLE `items` (
 
 DROP TABLE IF EXISTS `shippings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shippings` (
   `id` varchar(100) NOT NULL,
   `zip_code` varchar(100) NOT NULL,
@@ -121,18 +151,54 @@ CREATE TABLE `shippings` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `shippings`
+--
+
+LOCK TABLES `shippings` WRITE;
+/*!40000 ALTER TABLE `shippings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shippings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction_items`
+--
+
+DROP TABLE IF EXISTS `transaction_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `transaction_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `transaction_items_unique` (`transaction_id`),
+  KEY `transaction_items_history_items_FK` (`item_id`),
+  CONSTRAINT `transaction_items_history_items_FK` FOREIGN KEY (`item_id`) REFERENCES `history_items` (`item_id`),
+  CONSTRAINT `transaction_items_transactions_FK` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction_items`
+--
+
+LOCK TABLES `transaction_items` WRITE;
+/*!40000 ALTER TABLE `transaction_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `transactions`
 --
 
 DROP TABLE IF EXISTS `transactions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transactions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `manufacturer_user_id` varchar(100) NOT NULL,
   `purchaser_user_id` varchar(100) NOT NULL,
-  `item_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
   `tracking_id` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL,
   `zip_code` varchar(100) NOT NULL,
@@ -140,22 +206,24 @@ CREATE TABLE `transactions` (
   `phone_number` varchar(100) NOT NULL,
   `history_manufacturer_user_id` int(11) NOT NULL,
   `history_purchaser_user_id` int(11) NOT NULL,
-  `history_item_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `transactions_items_FK` (`item_id`),
   KEY `transactions_history_users_FK` (`history_manufacturer_user_id`),
   KEY `transactions_history_users_FK_1` (`history_purchaser_user_id`),
-  KEY `transactions_history_items_FK` (`history_item_id`),
   KEY `transactions_users_FK` (`manufacturer_user_id`),
   KEY `transactions_users_FK_1` (`purchaser_user_id`),
-  CONSTRAINT `transactions_history_items_FK` FOREIGN KEY (`history_item_id`) REFERENCES `history_items` (`id`),
   CONSTRAINT `transactions_history_users_FK` FOREIGN KEY (`history_manufacturer_user_id`) REFERENCES `history_users` (`id`),
-  CONSTRAINT `transactions_history_users_FK_1` FOREIGN KEY (`history_purchaser_user_id`) REFERENCES `history_users` (`id`),
-  CONSTRAINT `transactions_items_FK` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
-  CONSTRAINT `transactions_users_FK` FOREIGN KEY (`manufacturer_user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `transactions_users_FK_1` FOREIGN KEY (`purchaser_user_id`) REFERENCES `users` (`id`)
+  CONSTRAINT `transactions_history_users_FK_1` FOREIGN KEY (`history_purchaser_user_id`) REFERENCES `history_users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -163,7 +231,7 @@ CREATE TABLE `transactions` (
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` varchar(100) NOT NULL,
   `stripe_account_id` varchar(100) DEFAULT NULL,
@@ -171,10 +239,18 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `history_user_id` (`history_user_id`),
-  UNIQUE KEY `stripe_account_id` (`stripe_account_id`),
-  CONSTRAINT `users_history_users_FK` FOREIGN KEY (`history_user_id`) REFERENCES `history_users` (`id`)
+  UNIQUE KEY `stripe_account_id` (`stripe_account_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping routines for database 'CharisWorks'
@@ -189,4 +265,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-25 14:18:37
+-- Dump completed on 2024-02-08 17:39:14
