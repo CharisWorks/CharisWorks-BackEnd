9f7cb5caf7e481e1d03781fa1985d9bf5917c0db
WhatACotton <whatacotton@gmail.com>
2024-02-09 19:51:26 +0900

    add: example user

--- handler/user.go
+++ handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/charisworks/charisworks-backend/user"
 	"github.com/charisworks/charisworks-backend/validation"
 	"github.com/gin-gonic/gin"
 )
@@ -12,7 +13,8 @@ func (h *Handler) SetupRoutesForUser(firebaseApp *validation.FirebaseApp) {
 	UserRouter.Use(FirebaseMiddleware(*firebaseApp))
 	{
 		UserRouter.GET("/user", func(c *gin.Context) {
-			c.JSON(http.StatusOK, c.MustGet("UID").(string))
+			User := user.ExampleUser(c.MustGet("UID").(string))
+			c.JSON(http.StatusOK, User)
 		})
 	}
 }
--- user/example.go
+++ user/example.go
@@ -1,1 +1,32 @@
 package user
+
+import "time"
+
+func ExampleUser(UID string) User {
+	e := new(User)
+	e.UserId = UID
+
+	return *e
+
+}
+
+func ExampleUser2(UID string) User {
+	// ユーザー型の例を生成
+	user := User{
+		UserId: "12345",
+		UserProfile: &UserProfile{
+			DisplayName: "John Doe",
+			Description: "Example user",
+			CreatedAt:   time.Now(),
+		},
+		UserAddress: &UserAddress{
+			RealName:    "John Doe",
+			ZipCode:     "12345",
+			Address1:    "123 Main St",
+			Address2:    "Apt 4B",
+			Address3:    nil,
+			PhoneNumber: "555-1234",
+		},
+	}
+	return user
+}
