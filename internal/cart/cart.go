e72e8d138c7c5af508436b147aa2200378058e72
WhatACotton <whatacotton@gmail.com>
2024-02-11 15:05:57 +0900

    fix: rename ctx

--- internal/cart/cart.go
+++ internal/cart/cart.go
@@ -1,19 +1,16 @@
 package cart
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-func GetCart(i ICartRequest, ctx *gin.Context) ([]Cart, error) {
+func GetCart(i ICartRequest, ctx *gin.Context) (*[]Cart, error) {
 	Cart, err := i.Get(ctx)
-	return *Cart, err
+	return Cart, err
 }
 func PostCart(p CartRequestPayload, i ICartRequest, ctx *gin.Context) error {
 	err := i.Register(p, ctx)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, err)
 		return err
 	}
 	return err
@@ -22,7 +19,7 @@ func UpdateCart(p CartRequestPayload, i ICartRequest, ctx *gin.Context) error {
 	err := i.Update(p, ctx)
 	return err
 }
-func DeleteCart(itemId string, i ICartRequest, c *gin.Context) error {
-	err := i.Delete(itemId, c)
+func DeleteCart(itemId string, i ICartRequest, ctx *gin.Context) error {
+	err := i.Delete(itemId, ctx)
 	return err
 }
