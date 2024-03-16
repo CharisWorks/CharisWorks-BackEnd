2c6d3e97ade4ae0e5c0861d8d949208ebd101f50
whatacotton <whatacotton@gmail.com>
2024-03-16 23:38:27 +0900

    fix: error handling and delete images when item was deleted

--- internal/manufacturer/manufacturer_repository.go
+++ internal/manufacturer/manufacturer_repository.go
@@ -4,16 +4,18 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/charisworks/charisworks-backend/internal/images"
 	"github.com/charisworks/charisworks-backend/internal/items"
 	"github.com/charisworks/charisworks-backend/internal/utils"
 	"gorm.io/gorm"
 )
 
 type Repository struct {
-	DB *gorm.DB
+	DB   *gorm.DB
+	Crud images.R2Conns
 }
 
-func (m Repository) Register(itemId string, i RegisterPayload, userId string) error {
+func (r Repository) Register(itemId string, i RegisterPayload, userId string) error {
 	json, err := json.Marshal(i.Details.Tags)
 	if err != nil {
 		return &utils.InternalError{Message: utils.InternalErrorInvalidPayload}
@@ -29,24 +31,36 @@ func (m Repository) Register(itemId string, i RegisterPayload, userId string) er
 		Description:        i.Details.Description,
 		Tags:               string(json),
 	}
-	if err := m.DB.Create(&item).Error; err != nil {
+	if err := r.DB.Create(&item).Error; err != nil {
 		log.Print("DB error: ", err)
 		return &utils.InternalError{Message: utils.InternalErrorDB}
 	}
 	return nil
 }
 
-func (m Repository) Update(i map[string]interface{}, itemId string) error {
-	if err := m.DB.Table("items").Where("id = ?", itemId).Updates(i).Error; err != nil {
+func (r Repository) Update(i map[string]interface{}, itemId string) error {
+	if err := r.DB.Table("items").Where("id = ?", itemId).Updates(i).Error; err != nil {
 		log.Print("DB error: ", err)
 		return &utils.InternalError{Message: utils.InternalErrorDB}
 	}
 	return nil
 }
-func (m Repository) Delete(itemId string) error {
-	if err := m.DB.Table("items").Where("id = ?", itemId).Delete(&utils.Item{}).Error; err != nil {
+func (r Repository) Delete(itemId string) error {
+	if err := r.DB.Table("items").Where("id = ?", itemId).Delete(&utils.Item{}).Error; err != nil {
 		log.Print("DB error: ", err)
 		return &utils.InternalError{Message: utils.InternalErrorDB}
+	}
+
+	images, err := r.Crud.GetImages(itemId + "/")
+	if err != nil {
+		log.Print("R2 error: ", err)
+		return &utils.InternalError{Message: utils.InternalErrorR2}
+	}
+	for _, image := range images {
+		if err := r.Crud.DeleteImage(image); err != nil {
+			log.Print("R2 error: ", err)
+			return &utils.InternalError{Message: utils.InternalErrorR2}
+		}
 	}
 	return nil
 }
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/charisworks/charisworks-backend/internal/admin"
 	"github.com/charisworks/charisworks-backend/internal/cart"
 	"github.com/charisworks/charisworks-backend/internal/cash"
+	"github.com/charisworks/charisworks-backend/internal/images"
 	"github.com/charisworks/charisworks-backend/internal/items"
 	"github.com/charisworks/charisworks-backend/internal/manufacturer"
 	"github.com/charisworks/charisworks-backend/internal/transaction"
@@ -27,7 +28,8 @@ func main() {
 	r := gin.Default()
 	r.ContextWithFallback = true
 	r.MaxMultipartMemory = 8 << 20 // 8 MiB
-
+	R2Conns := images.R2Conns{Crud: nil}
+	R2Conns.Init()
 	utils.CORS(r)
 	h := handler.NewHandler(r)
 	app, err := validation.NewFirebaseApp()
@@ -39,7 +41,7 @@ func main() {
 		return
 	}
 	s := grpc.NewServer(
-	//grpc.UnaryInterceptor(admin.AuthUnaryServerInterceptor),
+		grpc.UnaryInterceptor(admin.AuthUnaryServerInterceptor),
 	)
 	userpb.RegisterUserServiceServer(s, &admin.UserServiceServer{})
 	userpb.RegisterItemServiceServer(s, &admin.ItemServiceServer{})
@@ -60,7 +62,7 @@ func main() {
 		cartRequests := cart.Requests{CartRepository: cart.Repository{DB: db}, CartUtils: cart.Utils{}}
 		itemRequests := items.Requests{ItemRepository: items.ItemRepository{DB: db}, ItemUtils: items.ItemUtils{}}
 		userRequests := users.Requests{UserUtils: users.UserUtils{}, UserRepository: users.UserRepository{DB: db}}
-		manufacturerRequests := manufacturer.Requests{ManufacturerItemRepository: manufacturer.Repository{DB: db}, ManufacturerInspectPayloadUtils: manufacturer.ManufacturerUtils{}, ItemRepository: items.ItemRepository{DB: db}}
+		manufacturerRequests := manufacturer.Requests{ManufacturerItemRepository: manufacturer.Repository{DB: db, Crud: R2Conns}, ManufacturerInspectPayloadUtils: manufacturer.ManufacturerUtils{}, ItemRepository: items.ItemRepository{DB: db}}
 		stripeRequests := cash.Requests{CartRequests: cartRequests, UserRequests: userRequests}
 		transactionRequests := transaction.TransactionRequests{TransactionRepository: transaction.Repository{DB: db}, CartRepository: cart.Repository{DB: db}, CartUtils: cart.Utils{}, StripeRequests: cash.Requests{CartRequests: cartRequests, UserRequests: userRequests}, StripeUtils: cash.Utils{}}
 		webhookRequests := transaction.Webhook{StripeUtils: cash.Utils{}, TransactionRepository: transaction.Repository{DB: db}, ItemUpdater: items.Updater{DB: db}, App: app.App}
@@ -68,9 +70,9 @@ func main() {
 		h.SetupRoutesForItem(itemRequests)
 		h.SetupRoutesForUser(app, userRequests)
 		h.SetupRoutesForCart(app, cartRequests, userRequests)
-		h.SetupRoutesForManufacturer(app, manufacturerRequests)
+		h.SetupRoutesForManufacturer(app, manufacturerRequests, userRequests)
 		h.SetupRoutesForStripe(app, userRequests, stripeRequests, transactionRequests)
-		h.SetupRoutesForImages(app, manufacturerRequests, itemRequests, userRequests)
+		h.SetupRoutesForImages(app, manufacturerRequests, itemRequests, userRequests, R2Conns)
 		h.Router.Run(":8080")
 	}()
 	// Ctrl+Cが入力されたらGraceful shutdownされるようにする
